87d650ae1463ee81dfa80b38a4dc2b013deeeca7
LuisMiguelCardenas <luismiguelcardenas2011@gmail.com>
2021-06-22 17:54:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
+  
+  const [data, setData] = useState('');
+  const calcBtns = [];
+  const arrayBtns = [0,1,2,4,5,6,7,8,9,'.',',','%']
+
+  arrayBtns.forEach(item => {
+    calcBtns.push(
+      <button className='col-3 m-1 btn btn-secondary' onClick = {e => {
+        setData(data +e.target.value)
+      }}
+      value={item}
+      key={item}
+      >
+        {item}
+      </button>
+    )
+  })
+
+  console.log(arrayBtns)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container m-5 justify-content-center row bg-dark py-5">
+      <div className='bg-light m-3 col-11'>{data}</div>
+      <div className='col-7 p-0'>{calcBtns}</div>
+      <div className='row justify-content-center col-5'>
+        <button className='btn btn-warning col-5 m-1' onClick = {e => setData(data + e.target.value)} value='+'>+</button>
+        <button className='btn btn-warning col-5 m-1' onClick = {e => setData(data + e.target.value)} value='-'>-</button>
+        <button className='btn btn-warning col-5 m-1' onClick = {e => setData(data + e.target.value)} value='*'>*</button>
+        <button className='btn btn-warning col-5 m-1' onClick = {e => setData(data + e.target.value)} value='/'>/</button>
+        <button className='btn btn-warning col-5 m-1' onClick = {e => {
+          try {
+            setData(
+              String(eval(data)).length > 3 && String(eval(data)).includes('.') ? String(eval(data).toFixed(4)) : String(eval(data))
+            );
+          }catch(err) {
+            console.log(err)
+          }
+        }} value='='>=
+
+        </button>
+      </div>
+      <button className='btn btn-danger col-2 my-3 mx-1' onClick={() => setData(data.substr(0,data.length -  1))}>clear</button>
+      <button className='btn btn-danger col-2 my-3 mx-1' onClick ={() => setData('')}>AC</button>
     </div>
   );
 }
-
+ 
 export default App;
